b11864e7acd192807651d430b7017a20440c0f71
obanimistudio-star <obanimistudio@gmail.com>
2026-09-14 20:28:25 +0100

    Refresh VoiceBridge offline cache

--- voicebridge/sw.js
+++ voicebridge/sw.js
@@ -1,4 +1,4 @@
-const CACHE="obanimivoicebridge-v1";
+const CACHE="obanimivoicebridge-v2";
 const ASSETS=["./","./index.html","./manifest.webmanifest","../assets/icons/app-icon-192.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
